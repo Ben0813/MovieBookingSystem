b31332e99da6e5164c28126a1aba1af30cfbe3a3
Benoit Dewigne <benoitdewigne@gmail.com>
2023-09-04 01:00:58 +0200

    first commit

---
+++ DataAndModifications.sql
@@ -0,0 +1,112 @@
+-- Insert data into MovieTheater table
+INSERT INTO MovieTheater (name, address) VALUES
+('CineComplex', '1000 Bruxelles'),
+('Movieland', '5030 Gembloux'),
+('CineTech', '5000 Namur') ;
+
+-- Insert data into Auditorium table
+INSERT INTO Auditorium (number, capacity, availableSeats, movieTheaterId) VALUES
+(1, 100, 100, 1),
+(2, 80, 80, 1),
+(3, 100, 100, 2);
+
+-- Insert data into Movie table
+INSERT INTO Movie (title, duration, genre) VALUES
+('The Dark Knight', 152, 'Action/Thriller'),
+('Forrest Gump', 142, 'Drame/Comédie'),
+('La Haine', 98, 'Drame');
+
+-- Insert data into Screening table
+INSERT INTO Screening (time, movieId, auditoriumId) VALUES
+('2023-09-02 19:00:00', 1, 2),
+('2023-09-02 21:00:00', 2, 3),
+('2023-09-04 23:00:00', 3, 4);
+
+-- Insert data into Rate table
+INSERT INTO Rate (type, price) VALUES
+('Full', 9.20),
+('Student', 7.60),
+('Child', 5.90);
+
+-- Insert data into Client table
+INSERT INTO Client (name, email) VALUES
+('Jacques Cousteau', 'deepsea@exemple.fr'),
+('François Pignon', 'quiproquo@exemple.fr'),
+('Sherlock Holmes', '221B@bakerstreet.uk');
+
+-- Insert data into Booking table
+INSERT INTO Booking (screeningId, rateId, clientId, paymentMethod) VALUES
+(5, 1, 2, 'Credit Card'),
+(4, 2, 1, 'Cash'),
+(6, 1, 3, 'Cash');
+
+-- Insert data into PaymentMethod table
+INSERT INTO PaymentMethod (method) VALUES
+('Credit Card'),
+('Cash'),
+('PayPal');
+
+-- Insert data into LocalAdmin table
+INSERT INTO LocalAdmin (name, movieTheaterId) VALUES
+('Jean-Luc Dupont', 3),
+('François Leroux', 2),
+('Jeroen Van der Veld', 1);
+
+-- Add columns to LocalAdmin table
+ALTER TABLE LocalAdmin
+ADD COLUMN firstName VARCHAR(255),
+ADD COLUMN lastName VARCHAR(255);
+
+-- Update data in LocalAdmin table
+UPDATE LocalAdmin SET firstName = 'Jean-Luc', lastName = 'Dupont' WHERE id = 1;
+UPDATE LocalAdmin SET firstName = 'François', lastName = 'Leroux' WHERE id = 2;
+UPDATE LocalAdmin SET firstName = 'Jeroen', lastName = 'Van der Wiel' WHERE id = 3;
+
+-- Remove column from LocalAdmin table
+ALTER TABLE LocalAdmin
+DROP COLUMN name;
+
+-- Empty LocalAdmin table
+TRUNCATE TABLE LocalAdmin;
+
+INSERT INTO LocalAdmin (firstName, lastName, movieTheaterId) VALUES
+('Jeroen', 'Van der Veld', 1),
+('François', 'Leroux', 2),
+('Jean-Luc', 'Dupont', 3);
+
+-- Add columns to SuperAdmin table
+ALTER TABLE SuperAdmin
+ADD COLUMN firstName VARCHAR(255),
+ADD COLUMN lastName VARCHAR(255);
+
+-- Remove column from SuperAdmin table
+ALTER TABLE SuperAdmin
+DROP COLUMN name;
+
+-- Insert data into SuperAdmin table
+INSERT INTO SuperAdmin (firstName, lastName) VALUES
+('Lex', 'Luthor');
+
+-- Add password column to LocalAdmin table
+ALTER TABLE LocalAdmin
+ADD COLUMN password VARBINARY(256) NOT NULL;
+
+-- Add password column to SuperAdmin table
+ALTER TABLE SuperAdmin
+ADD COLUMN password VARBINARY(256) NOT NULL;
+
+-- Update password data in LocalAdmin table
+UPDATE LocalAdmin SET password = AES_ENCRYPT('password', 'secret_key') WHERE id = 1;
+
+-- Update password data in SuperAdmin table
+UPDATE SuperAdmin SET password = AES_ENCRYPT('password', 'secret_key') WHERE id = 1;
+
+
+
+
+-- This command will create a backup of the MovieBookingSystem database and save it to the Desktop as a SQL file.
+mysqldump -u root -p MovieBookingSystem > ~/Desktop/backup.sql
+
+-- This command will restore the MovieBookingSystem database from the backup file located on the Desktop.
+mysql -u root -p MovieBookingSystem < ~/Desktop/backup.sql
+
---
+++ MovieBookingSystem.sql
@@ -0,0 +1,84 @@
+-- Creation of the database
+CREATE DATABASE MovieBookingSystem;
+USE MovieBookingSystem;
+
+-- Creation of the MovieTheater table
+CREATE TABLE MovieTheater (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
+
+-- Creation of the Auditorium table
+CREATE TABLE Auditorium (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    number INT NOT NULL,
+    capacity INT NOT NULL,
+    availableSeats INT NOT NULL,
+    movieTheaterId INT,
+    FOREIGN KEY (movieTheaterId) REFERENCES MovieTheater(id)
+);
+
+-- Creation of the Movie table
+CREATE TABLE Movie (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    duration INT NOT NULL,
+    genre VARCHAR(50)
+);
+
+-- Creation of the Screening table
+CREATE TABLE Screening (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    time DATETIME NOT NULL,
+    movieId INT,
+    auditoriumId INT,
+    FOREIGN KEY (movieId) REFERENCES Movie(id),
+    FOREIGN KEY (auditoriumId) REFERENCES Auditorium(id)
+);
+
+-- Creation of the Rate table
+CREATE TABLE Rate (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    type VARCHAR(50) NOT NULL,
+    price FLOAT NOT NULL
+);
+
+-- Creation of the Client table
+CREATE TABLE Client (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Creation of the Booking table
+CREATE TABLE Booking (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    screeningId INT,
+    rateId INT,
+    clientId INT,
+    paymentMethod VARCHAR(50),
+    FOREIGN KEY (screeningId) REFERENCES Screening(id),
+    FOREIGN KEY (rateId) REFERENCES Rate(id),
+    FOREIGN KEY (clientId) REFERENCES Client(id)
+);
+
+-- Creation of the PaymentMethod table
+CREATE TABLE PaymentMethod (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    method VARCHAR(50) NOT NULL
+);
+
+-- Creation of the LocalAdmin table
+CREATE TABLE LocalAdmin (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    movieTheaterId INT,
+    FOREIGN KEY (movieTheaterId) REFERENCES MovieTheater(id)
+);
+
+-- Creation of the SuperAdmin table
+CREATE TABLE SuperAdmin (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
